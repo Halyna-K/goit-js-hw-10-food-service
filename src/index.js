6848f51cb817c7ccdddd234ca869abbbf85e3746
Halyna-K <hkzlvs@gmail.com>
2021-08-12 22:33:24 +0300

    create menu page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
-import menuList from './menu.json';
-import menuTemplates from './templates/templatesMenu.hbs';
+import menuList from '/menu.json';
+import menuTemplates from './templates/libs.hbs';
 import './styles.css';
 
-console.log(menuList);
-console.log(menuTemplates);
-const menu = document.querySelector('.js-menu');
-const markup = menuList.map(menuTemplates).join('');
+// const imagesItemEl = document.createElement('li');
+// imagesItemEl.classList.add('gallery__item');
+// const imagesLinkEl = document.createElement('a');
+// imagesLinkEl.classList.add('gallery__link');
+// imagesLinkEl.href = `${galleryItem.original}`;
+// imagesLinkEl.insertAdjacentHTML(
+//   'afterbegin',
+//   `<img class= "gallery__image" src = ${galleryItem.preview} data-source = ${galleryItem.original} alt = ${galleryItem.description} width = 100%>`,
+// );
+
+// console.log(menuTemplates);
+const menu = document.querySelector('.menu');
+const markup =
+  // menuTemplates(menuList);
+  menuList.map(menuTemplates).join('');
 menu.insertAdjacentHTML('beforeend', markup);
+
+// console.log(menu);
