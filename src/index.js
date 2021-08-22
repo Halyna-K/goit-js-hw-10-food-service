61bf436f81d6073a9036b10d4ec439cfd520f9c9
Halyna-K <hkzlvs@gmail.com>
2021-08-22 15:57:09 +0300

    fixed Checkbox

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import menuList from '/menu.json';
+import menuList from './menu.json';
 import menuTemplates from './templates/menu.hbs';
 import { refs } from './js/refs.js';
-import { onChangeTheme } from './js/hundler.js';
+import { setSavedValue, onChangeTheme } from './js/hundler.js';
 import './styles.css';
 
 const markup = menuList.map(menuTemplates).join('');
 refs.menu.insertAdjacentHTML('beforeend', markup);
 
 refs.switch.addEventListener('change', onChangeTheme);
+setSavedValue(refs.switch);
--- src/js/hundler.js
+++ src/js/hundler.js
@@ -1,14 +1,24 @@
 import { refs, theme } from '../js/refs.js';
 
- export function onChangeTheme() {
-   const themeChecked = refs.switch.checked;
-   if (themeChecked) {
-    document.body.classList.add(theme.DARK);
-    document.body.classList.remove(theme.LIGHT);
-    localStorage.setItem('theme', theme.DARK);
-   } else {
-    document.body.classList.add(theme.LIGHT);
-    document.body.classList.remove(theme.DARK);
-    localStorage.setItem('theme', theme.LIGHT);
+   export function onChangeTheme() {
+       const themeChecked = refs.switch.checked;
+      if (themeChecked) {
+        document.body.classList.remove(theme.LIGHT);
+        document.body.classList.add(theme.DARK);
+        localStorage.setItem('theme', theme.DARK);
+      } else {
+        document.body.classList.remove(theme.DARK);
+        document.body.classList.add(theme.LIGHT);
+        localStorage.setItem('theme', theme.LIGHT);
+      }
+    }
+
+ export function setSavedValue(e){
+     if (localStorage.getItem('theme') === null) {
+       e.checked = true;
+       localStorage.setItem('theme', theme.DARK);
+     } else{
+       e.checked = localStorage.getItem('theme') == theme.DARK;
+       document.body.classList = localStorage.getItem('theme');
+     }
    }
- }
